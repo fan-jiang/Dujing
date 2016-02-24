eed11efac5225aecf30196eca56717989f33d66a
Fan Jiang <fjiang@ca.fujitsu.com>
2016-02-24 13:00:33 -0500

    Update build.bat to generate pdf file

--- build/build.bat
+++ build/build.bat
@@ -1,1 +1,6 @@
-pandoc ..\source\mengzi.txt -o mengzi_TraditionalFont_UnemphasizedName_FontSize48.html --standalone --toc --css=..\css\UnemphasizedName.css --css=..\css\FormatHeader.css --verbose
+Set Output=mencius_TraditionalFont_UnemphasizedName_FontSize48
+Set HTMLExt=.html
+Set HTMLOutput=%Output%%HTMLExt%
+
+Call pandoc ..\source\mencius.md -o %HTMLOutput% --standalone --toc --css=..\css\UnemphasizedName.css --css=..\css\FormatHeader.css --verbose
+call wkhtmltopdf.exe %HTMLOutput% %Output%.pdf
