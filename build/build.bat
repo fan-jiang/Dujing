dafd76a8d9679bb2eefebedd4d3ee665a9d8a755
fjiang <dizigui.fan@gmail.com>
2016-02-28 22:32:34 -0500

    Add buidall.bat

--- build/build.bat
+++ build/build.bat
@@ -2,14 +2,21 @@ Set DecorateName=DecorateName
 Set Book=mencius
 Set FontType=Traditional
 Set FontSize=FontSize48
-Set Output1=%Book%_%FontType%_%FontSize%_%DecorateName%
-Set Output2=%Book%_%FontType%_%FontSize%
+Set BookFont=%Book%_%FontType%_%FontSize%
+Set CSSExt=.css
+Set CSS_=--css=..\css\
+Set FontSizeStyle=%CSS_%FontSize%CSSExt%
+Set NameStyle=%CSS_%%DecorateName%%CSSExt%
+Set Output=%BookFont%
+Set Style=%FontSizeStyle% 
+
+If "%1" == "name" (
+	Set Output=%Output%_%DecorateName%
+	Set Style=%Style% %NameStyle%
+)
+
 Set HTMLExt=.html
-Set HTMLOutput1=%Output1%%HTMLExt%
-Set HTMLOutput2=%Output2%%HTMLExt%
+Set HTMLOutput=%Output%%HTMLExt%
 
-Call pandoc ..\source\%Book%.md -o %HTMLOutput1% --standalone --toc --css=..\css\%DecorateName%.css --css=..\css\FontSize.css --verbose
-call wkhtmltopdf.exe %HTMLOutput1% %Output1%.pdf
-
-Call pandoc ..\source\%Book%.md -o %HTMLOutput2% --standalone --toc --css=..\css\FontSize.css --verbose
-call wkhtmltopdf.exe %HTMLOutput2% %Output2%.pdf
+Call pandoc ..\source\%Book%.md -o %HTMLOutput% --standalone --toc %Style% --verbose
+Call wkhtmltopdf.exe %HTMLOutput% %Output%.pdf
---
+++ build/buildAll.bat
@@ -0,0 +1,5 @@
+TaskKill /f /im AcroRd32.exe
+Call build
+Call build name
+Del *.html
+Pause
