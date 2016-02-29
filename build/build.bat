9607f3e36f017e5575de730d8100435593f19fcb
fjiang <dizigui.fan@gmail.com>
2016-02-28 21:06:36 -0500

    Improve book variable

--- build/build.bat
+++ build/build.bat
@@ -1,14 +1,15 @@
 Set DecorateName=DecorateName
-Set Book=mencius_TraditionalFont
+Set Book=mencius
+Set FontType=Traditional
 Set FontSize=FontSize48
-Set Output1=%Book%_%FontSize%_%DecorateName%
-Set Output2=%Book%_%FontSize%
+Set Output1=%Book%_%FontType%_%FontSize%_%DecorateName%
+Set Output2=%Book%_%FontType%_%FontSize%
 Set HTMLExt=.html
 Set HTMLOutput1=%Output1%%HTMLExt%
 Set HTMLOutput2=%Output2%%HTMLExt%
 
-Call pandoc ..\source\mencius.md -o %HTMLOutput1% --standalone --toc --css=..\css\%DecorateName%.css --css=..\css\FontSize.css --verbose
+Call pandoc ..\source\%Book%.md -o %HTMLOutput1% --standalone --toc --css=..\css\%DecorateName%.css --css=..\css\FontSize.css --verbose
 call wkhtmltopdf.exe %HTMLOutput1% %Output1%.pdf
 
-Call pandoc ..\source\mencius.md -o %HTMLOutput2% --standalone --toc --css=..\css\FontSize.css --verbose
+Call pandoc ..\source\%Book%.md -o %HTMLOutput2% --standalone --toc --css=..\css\FontSize.css --verbose
 call wkhtmltopdf.exe %HTMLOutput2% %Output2%.pdf
