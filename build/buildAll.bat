6092dd2b29fb7bafb9b08666007445310bc62ca2
Fan Jiang <fjiang@ca.fujitsu.com>
2016-03-15 12:40:53 -0400

    Test code

--- build/buildAll.bat
+++ build/buildAll.bat
@@ -1,12 +1,12 @@
 TaskKill /f /im AcroRd32.exe
-::Call build
+Call build
 ::Call build name SongTi FontSize48
 ::Call build name QuanZiKuFont FontSize48
 ::Call build name BeiShiDaFont FontSize48
 ::Call build name JinWen FontSize48
 ::Call build name LiShu FontSize48
 ::Call build name JiaGuWen FontSize48
-Call build name JinWen FontSize96
+::Call build name JinWen FontSize96
 
 ::Del *.html
-Pause
+start mencius_Traditional.pdf
