f9162108af523cddeb178c53289af439b8f7e747
Fan Jiang <fan.jiang@ca.fujitsu.com>
2016-03-20 11:39:42 -0400

    Add log file

--- build/Build.bat
+++ build/Build.bat
@@ -1,14 +1,14 @@
-:: Stop PDF reader
-TaskKill /f /im AcroRd32.exe
-
-Set OpenCCFolder=..\opencc-0.4.2\
 Set SourceFolder=..\Source\
 Set BookExt=.md
 Set T=正體
 Set S=簡體
-
-Call GenerateBook 孟子
-Call GenerateBook 中庸
+Set BuildLog=BuildLog.txt
+@echo Build begins at: %DATE% %TIME% > %BuildLog%
+Set STARTTIME=%TIME%
+:: Stop PDF reader
+TaskKill /f /im AcroRd32.exe
+Call GenerateBook 孟子 >> %BuildLog%
+Call GenerateBook 中庸 >> %BuildLog%
 
 ::Call build name SongTi FontSize48
 ::Call build name QuanZiKuFont FontSize48
@@ -18,5 +18,43 @@ Call GenerateBook 中庸
 ::Call build name JiaGuWen FontSize48
 ::Call build name JinWen FontSize96
 
+:: Clean up
 Del *.html
 Del %SourceFolder%*%S%%BookExt%
+
+@echo off
+Set ENDTIME=%TIME%
+:: Change formatting for the start and end times
+for /F "tokens=1-4 delims=:.," %%a in ("%STARTTIME%") do (
+   set /A "start=(((%%a*60)+1%%b %% 100)*60+1%%c %% 100)*100+1%%d %% 100"
+)
+
+for /F "tokens=1-4 delims=:.," %%a in ("%ENDTIME%") do (
+   set /A "end=(((%%a*60)+1%%b %% 100)*60+1%%c %% 100)*100+1%%d %% 100"
+)
+
+:: Calculate the elapsed time by subtracting values
+set /A elapsed=end-start
+
+:: Format the results for output
+set /A hh=elapsed/(60*60*100), rest=elapsed%%(60*60*100), mm=rest/(60*100), rest%%=60*100, ss=rest/100, cc=rest%%100
+if %hh% lss 10 set hh=0%hh%
+if %mm% lss 10 set mm=0%mm%
+if %ss% lss 10 set ss=0%ss%
+if %cc% lss 10 set cc=0%cc%
+
+Set DURATION=%hh%:%mm%:%ss%,%cc%
+
+echo Start    : %STARTTIME%
+echo Finish   : %ENDTIME%
+echo          ---------------
+echo Duration : %DURATION% 
+
+@echo Build ends at: %DATE% %TIME% >> %BuildLog%
+
+
+
+
+
+
+
--- build/GenerateBook.bat
+++ build/GenerateBook.bat
@@ -1,7 +1,8 @@
 Set BookName=%1_
 Set BookPath=%SourceFolder%%BookName%
 :: Generate the simplified source file
-%OpenCCFolder%opencc -i %BookPath%%T%%BookExt% -o %BookPath%%S%%BookExt% -c %OpenCCFolde%zht2zhs.ini
+Set OpenCCFolder=..\opencc-0.4.2\
+Call %OpenCCFolder%opencc -i %BookPath%%T%%BookExt% -o %BookPath%%S%%BookExt% -c %OpenCCFolde%zht2zhs.ini
 
 Set OutputFolder=..\ÖÐÎÄ½›µä\%1
 Call GenerateBookHelper %T%
