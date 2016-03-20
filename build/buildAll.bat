6bed908c094c94709834b1c82d7ee94ea278ebac
Fan Jiang <fan.jiang@ca.fujitsu.com>
2016-03-20 09:11:58 -0400

    Include the text conversion in buildall.bat

--- build/buildAll.bat
+++ build/buildAll.bat
@@ -1,12 +1,25 @@
+:: Stop PDF reader
 TaskKill /f /im AcroRd32.exe
+
+:: Convert to the simplified text
+Set OpenCCFolder=..\opencc-0.4.2\
+Set SourceFolder=..\Source\
+Set BookExt=.md
 Set T=Traditional
 Set S=Simplified
 Set BookName=Mencius_
+Set BookPath=%SourceFolder%%BookName%
+%OpenCCFolder%opencc -i %BookPath%%T%%BookExt% -o %BookPath%%S%%BookExt% -c %OpenCCFolde%zht2zhs.ini
+
+:: Generate Book
 Set Book=%BookName%%T%
 Call build %Book%
 Set Book=%BookName%%S%
 Call build %Book%
+
 Set BookName=ZhongYong_
+Set BookPath=%SourceFolder%%BookName%
+%OpenCCFolder%opencc -i %BookPath%%T%%BookExt% -o %BookPath%%S%%BookExt% -c %OpenCCFolde%zht2zhs.ini
 Set Book=%BookName%%T%
 Call build %Book%
 Set Book=%BookName%%S%
--- source/TraditionToSimplify.bat
+++
@@ -1,7 +0,0 @@
-Set OpenCCFolder=..\opencc-0.4.2\
-Set BookExt=.md
-Set Book=Mencius_
-%OpenCCFolde%zht2zhs.ini
-%OpenCCFolder%opencc -i %Book%Traditional%BookExt% -o %Book%Simplified%BookExt% -c %OpenCCFolde%zht2zhs.ini
-Set Book=ZhongYong_
-%OpenCCFolder%opencc -i %Book%Traditional%BookExt% -o %Book%Simplified%BookExt% -c %OpenCCFolde%zht2zhs.ini
