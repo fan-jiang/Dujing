21e005c7e603df0402c669b65279e67198781708
Fan Jiang <fan.jiang@ca.fujitsu.com>
2016-03-20 09:25:13 -0400

    Rename files

--- build/GenerateBook.bat
+++ build/GenerateBook.bat
@@ -2,6 +2,6 @@ Set BookName=%1_
 Set BookPath=%SourceFolder%%BookName%
 %OpenCCFolder%opencc -i %BookPath%%T%%BookExt% -o %BookPath%%S%%BookExt% -c %OpenCCFolde%zht2zhs.ini
 Set Book=%BookName%%T%
-Call build %Book%
+Call Generate %Book%
 Set Book=%BookName%%S%
-Call build %Book%
+Call Generate %Book%
