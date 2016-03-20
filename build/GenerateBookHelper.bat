735dcee9b369a241f3c8318edd6655e3d689ae7c
Fan Jiang <fan.jiang@ca.fujitsu.com>
2016-03-20 16:01:33 -0400

    Generate the fonts which can be generated completely

--- build/GenerateBookHelper.bat
+++ build/GenerateBookHelper.bat
@@ -1,11 +1,12 @@
 Set Book=%BookName%%1
 Call Generate %Book% name SongTi FontSize48
 Call Generate %Book% name QuanZiKuFont FontSize48
-Call Generate %Book% name BeiShiDaFont FontSize48
-Call Generate %Book% name JinWen FontSize48
-Call Generate %Book% name LiShu FontSize48
-Call Generate %Book% name JiaGuWen FontSize48
-Call Generate %Book% name JinWen FontSize96
+:: Call Generate %Book% name KaiTi FontSize48
+:: Call Generate %Book% name BeiShiDaFont FontSize48
+:: Call Generate %Book% name JinWen FontSize48
+:: Call Generate %Book% name LiShu FontSize48
+:: Call Generate %Book% name JiaGuWen FontSize48
 Set Destinaton=%OutputFolder%\%1
+RmDir %Destinaton% /s /q
 MkDir %Destinaton%
 Move /Y %Book%*.pdf %Destinaton%
