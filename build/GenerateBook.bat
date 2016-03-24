ffaef205aa61448ce5608792690d6aef7e9f2988
fjiang <dizigui.fan@gmail.com>
2016-03-23 20:50:30 -0400

    Change output folder

--- build/GenerateBook.bat
+++ build/GenerateBook.bat
@@ -4,7 +4,7 @@ Set BookPath=%SourceFolder%%BookName%
 Set OpenCCFolder=..\opencc-0.4.2\
 Call %OpenCCFolder%opencc -i %BookPath%%T%%BookExt% -o %BookPath%%S%%BookExt% -c %OpenCCFolde%zht2zhs.ini
 
-Set OutputFolder=..\ÖÐÎÄ½›µä\%1
+Set OutputFolder=%1
 Call GenerateBookHelper %T%
 Call GenerateBookHelper %S%
 
