63fbbd36bc9123fc8fe9ed41481dd36373959549
Fan Jiang <fjiang@ca.fujitsu.com>
2016-04-14 13:22:21 -0400

    Read a file from Python

--- Helper/Helper/Helper.py
+++ Helper/Helper/Helper.py
@@ -1,5 +1,12 @@
 def main():
-    print ("Hello world!")
+    try:
+        fileName = "MengZi_Traditional.md"
+        filePath = "../../source/" + fileName
+        with open(filePath, 'r') as file:
+            for line in file:
+                print line
+    except IOError:
+        print ("The file (" + filePath + ") does not exist.")
 
 if __name__ == '__main__':
     main()
