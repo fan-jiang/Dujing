77e09f2f085bc894c1f45e94662e32a981e9b0db
Fan Jiang <fjiang@ca.fujitsu.com>
2016-04-15 13:14:31 -0400

    Convert Chinese quotation

--- PythonScript/Helper/Helper.py
+++ PythonScript/Helper/Helper.py
@@ -1,12 +1,20 @@
+# This Python file uses the following encoding: utf-8
 def main():
     try:
         fileName = "MengZi_Traditional.md"
         filePath = "../../source/" + fileName
-        with open(filePath, 'r') as file:
-            for line in file:
-                print line
+        content = None
+        with open(filePath,'r') as file:
+            content = file.read().decode("utf-8")
+        content = content.replace(u"「",u'“')
+        content = content.replace(u"」",u'”')
+        content = content.replace(u"『",u'‘')
+        content = content.replace(u"』",u'’')
+        with open(filePath,'w') as file:
+            file.write(content.encode("utf-8"))
+        print "OK"
     except IOError:
-        print ("The file (" + filePath + ") does not exist.")
+        print ("IOError occurs while handling the file (" + filePath + ").")
 
 if __name__ == '__main__':
     main()
