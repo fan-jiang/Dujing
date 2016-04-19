0106ce5562300cfcb40e825f63c313345b4362fe
Fan Jiang <fjiang@ca.fujitsu.com>
2016-04-19 12:06:30 -0400

    Add unit test

---
+++ PythonScript/Helper/testConvert.py
@@ -0,0 +1,13 @@
+# This Python file uses the following encoding: utf-8
+
+# Somehow, running this unit test in VS2015 is very slow (6 seconds).
+# It is much faster to use command line "python -m unittest discover ." to do unit test 
+import unittest
+import Helper
+
+class TestConvert(unittest.TestCase):
+    def test_Convert(self):
+        self.assertEquals(Helper.Convert(u"『"), u"‘");
+
+if __name__ == '__main__':
+    unittest.main()
