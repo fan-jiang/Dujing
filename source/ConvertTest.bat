78fe65362ab8d1b5e9c5f64821c3b63bd87c3b0e
fjiang <dizigui.fan@gmail.com>
2016-04-27 22:53:36 -0400

    Update ConvertTest

--- source/ConvertTest.bat
+++
@@ -1,5 +0,0 @@
-Set OpenCCFolder=..\opencc-0.4.2\
-Set BookExt=.md
-Set BookPath=..\%1
-Set S=Simplified
-Call %OpenCCFolder%opencc -i %1 -o Test_Simplified.md -c %OpenCCFolde%zht2zhs.ini
---
+++ source/ConvertToSimplifiedChinese.bat
@@ -0,0 +1,4 @@
+Set OpenCCFolder=..\opencc-0.4.2\
+Set BookExt=.md
+Set S=Simplified
+Call %OpenCCFolder%opencc -i %1 -o %1_%S%%BookExt% -c %OpenCCFolde%zht2zhs.ini
