0a82a57551ae095b4cb8db989c6cc1870f53d471
Fan Jiang <fjiang@ca.fujitsu.com>
2016-04-28 12:22:02 -0400

    Include review comment from Li, Tao on Daxue

--- PythonScript/Helper/Convert.py
+++ PythonScript/Helper/Convert.py
@@ -30,7 +30,9 @@ tradtionalToSimplified = {
         u"遏籴":u"遏糴",
         u"逾":u"踰",
 ##　Lunyu
-        u"後彫":u"后凋",
+        u"后凋":u"后彫",
+## Daxue
+        u"恂栗":u"恂慄",
 }
 
 def Convert(content):
