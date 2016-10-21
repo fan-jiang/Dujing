3fb3a9bb4db65be284033053960f67aed14ec239
Fan Jiang <fjiang>
2016-10-21 12:49:13 -0400

    Make pandoc runnable

--- PythonScript/GenerateBook.py
+++ PythonScript/GenerateBook.py
@@ -1,15 +1,17 @@
 import subprocess
 def GenerateCover():
-	Cover = "Cover"
-	BookName = "BookName"
-	BookCover = BookName + Cover
-	BookCoverHTML = BookCover + ".html"
-	CSS = "CSS_"
-	CSSExt = "CSSExt"
+	#Cover = "Cover"
+	#BookName = "BookName"
+	#BookCover = BookName + Cover
+	#BookCoverHTML = BookCover + ".html"
+	#CSS = "CSS_"
+	#CSSExt = "CSSExt"
 
-	pandocCommand = "pandoc ..\\source\\" + BookCover + ".txt -o "
-	+ BookCoverHTML + " -standalone " + CSS_ + Cover + ".css --verbose"
-	subprocess.call(pandocCommand, stdout=FNULL, stderr=FNULL, shell=False)
+	#pandocCommand = "pandoc ..\\source\\" + BookCover + ".txt -o "
+	#+ BookCoverHTML + " -standalone " + CSS_ + Cover + ".css --verbose"
+	pandocCommand = ["pandoc", "..\\source\\test.txt", "-o", "test.html"]
+	with open('dujing.log', 'w') as trace:
+		subprocess.call(pandocCommand, stdin=None, stdout=trace, stderr=None, shell=True)
 
 if __name__ == '__main__':
 	GenerateCover()
