9e018e30b8cd57a574e359f3526e61fd998a36db
Fan Jiang <fjiang>
2016-10-24 12:35:27 -0400

    Introduce GenerateBook() to replace build.bat

--- PythonScript/GenerateBook.py
+++ PythonScript/GenerateBook.py
@@ -1,4 +1,6 @@
 import subprocess
+import os
+
 def GenerateCover():
 	#Cover = "Cover"
 	#BookName = "BookName"
@@ -9,9 +11,18 @@ def GenerateCover():
 
 	#pandocCommand = "pandoc ..\\source\\" + BookCover + ".txt -o "
 	#+ BookCoverHTML + " -standalone " + CSS_ + Cover + ".css --verbose"
-	pandocCommand = ["pandoc", "..\\source\\test.txt", "-o", "test.html"]
+	pandocCommand = "pandoc ..\\source\\test.txt -o test.html"
 	with open('dujing.log', 'w') as trace:
 		subprocess.call(pandocCommand, stdin=None, stdout=trace, stderr=None, shell=True)
 
+def GenerateBook():
+	Books = ["DaXue", "ZhongYong", "LunYu", "MengZi"]
+	prefaceCommand = "pandoc ..\\Source\\Preface.md -o Preface.html --standalone"
+	with open("Dujing.log", 'w') as trace:
+		subprocess.call(prefaceCommand, stdin=None, stdout=trace, stderr=None, shell=True)
+		os.chdir("..\\Build")
+		for book in Books:
+			subprocess.call("GenerateBook " + book, stdin=None, stdout=trace, stderr=None, shell=True)
+
 if __name__ == '__main__':
-	GenerateCover()
+	GenerateBook()
