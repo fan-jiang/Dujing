17685a78457a996a0ebdf6b69b2b9f2761bfafb3
Fan Jiang <fjiang>
2016-10-27 12:34:54 -0400

    Delete GenerateCover()

--- PythonScript/GenerateBook.py
+++ PythonScript/GenerateBook.py
@@ -1,19 +1,6 @@
 import subprocess
 import os
 
-def GenerateCover():
-	#Cover = "Cover"
-	#BookName = "BookName"
-	#BookCover = BookName + Cover
-	#BookCoverHTML = BookCover + ".html"
-	#CSS = "CSS_"
-	#CSSExt = "CSSExt"
-
-	#pandocCommand = "pandoc ..\\source\\" + BookCover + ".txt -o "
-	#+ BookCoverHTML + " -standalone " + CSS_ + Cover + ".css --verbose"
-	pandocCommand = "pandoc ..\\source\\test.txt -o test.html"
-	with open('dujing.log', 'w') as trace:
-		subprocess.call(pandocCommand, stdin=None, stdout=trace, stderr=None, shell=True)
 
 def GenerateBook():
 	Books = ["DaXue", "ZhongYong", "LunYu", "MengZi"]
