97df555e0231c77608d3b5f8a0c09e4b304246a6
Fan Jiang <dizigui.fan@gmail.com>
2021-02-27 23:38:43 -0500

    Generate books explicitly

--- build/GenerateBookHelper.bat
+++ build/GenerateBookHelper.bat
@@ -1,19 +1,17 @@
 Set Book=%BookName%%1
 Call Generate %Book% KaiTi FontSize48 Name
-Call Generate %Book%
+Call Generate %Book% KaiTi
 Call Generate %Book% SongTi
 
-:: Call Generate %Book% name QuanZiKuFont FontSize48
-
 :: The following fonts generated unreadable characters.
-:: Call Generate %Book% name FangSong FontSize48
-:: Call Generate %Book% name KaiTi FontSize48
+REM Call Generate %Book% FangSong
+REM Call Generate %Book% QuanZiKuFont
 
 :: The following fonts generated incomplete text.
-REM Call Generate %Book% name BeiShiDaFont FontSize48
-REM Call Generate %Book% name JinWen FontSize48
-REM Call Generate %Book% name LiShu FontSize48
-REM Call Generate %Book% name JiaGuWen FontSize48
+REM Call Generate %Book% BeiShiDaFont
+REM Call Generate %Book% JinWen
+REM Call Generate %Book% LiShu
+REM Call Generate %Book% JiaGuWen
 
 Set Destination=%OutputFolder%\%1
 RmDir %Destination% /s /q
