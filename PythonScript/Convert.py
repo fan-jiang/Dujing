ac7ae2241c8fdd09d35605f4ca97c479f5d59230
Fan Jiang <dizigui.fan@gmail.com>
2021-10-10 08:18:08 -0400

    working

--- PythonScript/Convert.py
+++ PythonScript/Convert.py
@@ -62,7 +62,7 @@ def remove_redundant_subscript(content):
 		i += 1
 	return content
 
-checkedToneChars = ["入"]
+checkedToneChars = "入日"
 
 # 入聲字： 用`^^`標誌, 比如： ^曰^。This is interpreted by [pandoc subscription 
 #　extension](https://pandoc.org/MANUAL.html#extension-superscript-subscript).
