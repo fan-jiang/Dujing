e704223479acaaa3b6e9869fb53975dc0fe502a2
Fan Jiang <dizigui.fan@gmail.com>
2021-10-10 08:24:32 -0400

    working

--- PythonScript/testConvert.py
+++ PythonScript/testConvert.py
@@ -8,18 +8,17 @@ import unittest
 import Convert
 
 class TestConvert(unittest.TestCase): 
-    def test_convert(self):
-        self.assertEqual(Convert.Convert(u"『"), u"‘")
-    def test_remove_redundant_subscript(self):
-        self.assertEqual("", Convert.remove_redundant_subscript(""))
-        self.assertEqual("b", Convert.remove_redundant_subscript("b~b~"))
-        self.assertEqual("b~", Convert.remove_redundant_subscript("b~"))
-        self.assertEqual("abc", Convert.remove_redundant_subscript("ab~b~c"))
-        self.assertEqual(u"舍", Convert.Convert(u"舍~舍~"))
-    def test_add_superscript(self):
-        self.assertEqual("", Convert.decorate_checkedToneCharacter_with_superscript(""))
-        self.assertEqual(u"^入^", Convert.decorate_checkedToneCharacter_with_superscript(u"入"))
-        self.assertEqual(u"^日^", Convert.decorate_checkedToneCharacter_with_superscript(u"日"))
+	def test_convert(self):
+		self.assertEqual(Convert.Convert(u"『"), u"‘")
+	def test_remove_redundant_subscript(self):
+		self.assertEqual("", Convert.remove_redundant_subscript(""))
+		self.assertEqual("b", Convert.remove_redundant_subscript("b~b~"))
+		self.assertEqual("b~", Convert.remove_redundant_subscript("b~"))
+		self.assertEqual("abc", Convert.remove_redundant_subscript("ab~b~c"))
+		self.assertEqual(u"舍", Convert.Convert(u"舍~舍~"))
+	def test_add_superscript(self):
+		self.assertEqual("", Convert.decorate_checkedToneCharacter_with_superscript(""))
+		self.assertEqual(u"^入^", Convert.decorate_checkedToneCharacter_with_superscript(u"入"))
 
 if __name__ == '__main__':
-    unittest.main()
+	unittest.main()
