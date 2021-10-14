d28508972d1dd05763952c68307292fcafcf4106
Fan Jiang <dizigui.fan@gmail.com>
2021-10-14 05:55:55 -0400

    working

--- PythonScript/CheckedToneChars.py
+++ PythonScript/CheckedToneChars.py
@@ -1,3 +1,6 @@
 # This Python file uses the following encoding: utf-8
 
-checkedToneChars = u"入日"
+#　reference:　http://wx.shangdiguo.com/blog.asp?wenzhangid=35658
+
+checkedToneChars = u"入日" \
+	"test" \
