e3448b3e6ef769e6473ae1d1561684aae9c761d2
Fan Jiang <dizigui.fan@gmail.com>
2021-10-14 06:50:49 -0400

    Update traditional text

--- build/GenerateBook.bat
+++ build/GenerateBook.bat
@@ -5,10 +5,13 @@ Set S=Simplified
 Set BookName=%1_
 Set BookPath=%SourceFolder%%BookName%
 
+:: Update the traditional source file
+Set TraditionalText=%BookPath%%T%%BookExt%
+Call ..\PythonScript\UpdateFile.py %T% %TraditionalText%
 :: Generate the simplified source file
 Set OpenCCFolder=..\opencc-0.4.2\
 Set SimplifiedText=%BookPath%%S%%BookExt%
-Call %OpenCCFolder%opencc -i %BookPath%%T%%BookExt% -o %SimplifiedText% -c %OpenCCFolder%zht2zhs.ini
+Call %OpenCCFolder%opencc -i %TraditionalText% -o %SimplifiedText% -c %OpenCCFolder%zht2zhs.ini
 Call ..\PythonScript\UpdateFile.py %S% %SimplifiedText%
 
 :: Generate book
