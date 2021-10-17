f805b0914b3f8fe1e42c9fb362f1666769193caa
Fan Jiang <dizigui.fan@gmail.com>
2021-10-17 06:07:10 -0400

    working

--- PythonScript/Convert.py
+++ PythonScript/Convert.py
@@ -79,15 +79,14 @@ def wrap_checked_tone_char_with_superscript_sign(content):
     result = ""
     for i, c in enumerate(content):
         if is_checked_tone_char(c) and not is_alternative_char(content, i):
-            result += wrap_with_superscript_sign(c)
+            result += wrap_with_pandoc_superscript_sign(c)
         else:
             result += c
     return result
 
 
-def wrap_with_superscript_sign(c):
-    superscriptSignInPandoc = '^'
-    return superscriptSignInPandoc + c + superscriptSignInPandoc
+def wrap_with_pandoc_superscript_sign(c):
+    return '^' + c + '^'
 
 
 def is_checked_tone_char(c):
