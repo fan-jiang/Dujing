701665bf529f2cd33e6ab4380bd6709fce8ce9eb
Fan Jiang <dizigui.fan@gmail.com>
2021-10-19 06:37:16 -0400

    working

--- PythonScript/Convert.py
+++ PythonScript/Convert.py
@@ -85,7 +85,7 @@ class CheckedToneMarker:
         self.book = book
         self.index = index
 
-    def mark_checked_tone_chars(book):
+    def mark_checked_tone_chars():
         result = ""
         for index, _ in enumerate(book):
             result += CheckedToneMarker(book, index).mark_checked_tone_char()
